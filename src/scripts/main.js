feedded81f994f6de2a269f6340a5e3bf5245e85
William Silva <williamreis.wsilva@gmail.com>
2023-07-09 23:38:55 -0300

    Ajustando responsividade

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,93 @@
+// ======= Variaveis ======= //
+
 const linksMenu = document.querySelectorAll(".header__Menu__links__item__link");
 const togglesMenu = document.querySelectorAll(".header__Menu__links__item__toggle");
 const carouselItens  = document.querySelectorAll(".carousel-item");
+const dropsMenu = document.querySelector(".header__Menu__drops");
+const dropLinks = document.querySelector(".header__Menu__links");
+let posicaoScrollAtual = 0
 
+// ======= Eventos ======= //
 
 for (let i = 0; i < linksMenu.length; i++) {
     linksMenu[i].addEventListener("click", function(link){
         
         desativarLinks();
         AtivarLink(link);
+        
     });
 }
 
+dropsMenu.addEventListener("click", function(){
+    if (dropLinks.style.display === "none") {
+        dropLinks.style.display = "block";
+      } else {
+        dropLinks.style.display = "none";
+    }
+})
+
+
+
+// Captura a seção atual com base na posição na janela
+document.addEventListener("DOMContentLoaded", function () {
+    const secaoFilmes = document.querySelector(".Filmes__carousel").getBoundingClientRect();
+    const livros = document.querySelector(".livros").getBoundingClientRect();
+    const games = document.querySelector(".games").getBoundingClientRect();
+    const autora = document.querySelector(".autora").getBoundingClientRect();
+    const footer = document.querySelector(".footer").getBoundingClientRect();
+    
+    document.addEventListener("scroll", function(){
+        posicaoScrollAtual = window.scrollY;
+        console.log(`autora: ${autora.top} footer: ${footer.top} Atual: ${posicaoScrollAtual}`)
+
+
+        // Verificar a posição atual do scroll
+        const secoes = [
+            { nome: "Filmes", top: secaoFilmes.top },
+            { nome: "Livros", top: livros.top },
+            { nome: "Games", top: games.top },
+            { nome: "Autora", top: autora.top },
+            { nome: "Footer", top: footer.top }
+          ];
+          
+          for (const secao of secoes) {
+            if (posicaoScrollAtual > secao.top) {
+              console.log(`Dentro da seção ${secao.nome} ${secao.top} `);
+            }
+        }
+    })
+  });
+
+
+  
+  
+  
+
+
+
+
+// $(document).ready(function() {
+//     $("#carousel_imagens").slick({autoplay: true});
+// })
+
+$(".footer__contato__email_newsletter").validate({
+    rules:{
+        email:{required: true, email: true}},
+        messages:{nome: "Por favor, insira seu nome"},
+        submitHandler:function(form) {
+            form.submit();
+        },
+        invalidHandler:function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }    
+})
+
+
+// ======= Funções ======= //
 
 function AtivarLink(link){
     const linkAtivo = link.target.parentNode.querySelector(".header__Menu__links__item__link");
@@ -26,21 +103,3 @@ function desativarLinks(){
         togglesMenu[i].classList.remove("header__Menu__links__item__toggle--is-active");
     }
 }
-
-
-    
-    setInterval(function() {
-        console.log("Olá mundo")
-      }, 10000); // Executa a cada 1 segundo (1000 milissegundos)
-    
-
-
-
-
-
-
-
-
-$(document).ready(function() {
-    $("#carousel_imagens").slick({autoplay: true});
-})
